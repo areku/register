a9f8355149fa357d355a392b0597045882145e81
Alexander Weigl <spamling@web.de>
2015-03-17 01:49:02 +0100

    add tests and readme

--- test.py
+++ test.py
@@ -1,21 +1,47 @@
 
 from register import *
 
+from unittest import *
+
+class BaseTest(TestCase):
+    def test_register(self):
+        # create registry
+        r = Registry()
 
 
-def test_a():
-    r = FunctionRegister()
+        @r
+        def abc():
+            pass
 
-    @r
-    def abc():
-        pass
+        @r(a = 2, b = 3)
+        def de():
+            pass
+
+        @r("test")
+        def a(): pass
 
 
-    @r(a = 2, b = 3)
-    def de():
-        pass
+        self.assertTrue( "abc" in r)
+        self.assertTrue( "de" in r)
+        self.assertTrue( "test" in r)
 
-    @r("test")
-    def a(): pass
+        self.assertIs(abc, r.lookup("abc"))
+        self.assertIs(de, r.lookup("de"))
+        self.assertIs(a, r.lookup("test"))
 
-    print r._register
+        self.assertDictEqual(dict(a=2,b=3), r.meta("de"))
+
+    def test_class_register(self):
+        r = Registry()
+        @r
+        class A(object): pass
+
+        @r("B")
+        class A(object): pass
+
+        self.assertTrue( "A" in r)
+        self.assertTrue( "B" in r)
+
+        self.assertIs(A, r.lookup("B"))
+        self.assertIsNot(A, r.lookup("A"))
+
